96b203c00256efb9d6ea08f5dc9eca659663e6c6
Becca Harris <beccalynharris@gmail.com>
2018-02-28 00:35:13 -0700

    add start button and get timer working on button click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
+  $("#start-button").on("click", function(){
+    $(this).hide();
+    startGame(); 
+  })
+})
+// *************************** // 
+// Function to start the timer // 
+// *************************** // 
+function startGame () {
   var timeRemaining = 30;
-  var timerElement = $('#timer');
   var timerId = setInterval(countdown, 1000);
-
+  var timerElement = $('#timer');
   function countdown() {
     if (timeRemaining == -1) {
       clearTimeout(timerId);
     } else {
-      timerElement.html(timeRemaining + ' seconds remaining!')
+      timerElement.html('Time Remaining: ' + timeRemaining + ' seconds')
       timeRemaining--;
     }
   }
+}
 
 
-
-
-
-
-
-
-
-
-})
